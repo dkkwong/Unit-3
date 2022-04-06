a413a0ad2799a44a02ff395bd7a5a7edbea8bceb
dkkwong <davidkwong6@gmail.com>
2022-04-06 13:01:27 -0500

    Activity 9

--- Unit 3/js/main.js
+++ Unit 3/js/main.js
@@ -1,154 +1,67 @@
-//execute script when window is loaded
-window.onload = function(){
-    var w = 900, h = 500;
+//begin script when window loads
+window.onload = setMap();
 
-    var container = d3.select("body") //get the <body> element from the DOM
-        .append("svg") //put a new svg in the body
-        .attr("width", w) //assign the width
-        .attr("height", h) //assign the height
-        .attr("class", "container") //always assign a class (as the block name) for styling and future selection
-        .style("background-color", "rgba(0,0,0,0.2)");
+//set up choropleth map
+function setMap(){
 
-    //innerRect block
-    var innerRect = container.append("rect")
-    .datum(400) //a single value is a DATUM
-    .attr("width", function(d){ //rectangle width
-        return d * 2; //400 * 2 = 800
-    })
-    .attr("height", function(d){ //rectangle height
-        return d; //400
-    })
-    .attr("class", "innerRect") //class name
-    .attr("x", 50) //position from left on the x (horizontal) axis
-    .attr("y", 50) //position from top on the y (vertical) axis
-    .style("fill", "#FFFFFF"); //fill color
+    //map frame dimensions
+    var width = 880,
+        height = 500;
 
+    //create new svg container for the map
+    var map = d3.select("body")
+        .append("svg")
+        .attr("class", "map")
+        .attr("width", width)
+        .attr("height", height);
 
-    var cityPop = [
-        { 
-            city: 'Madison',
-            population: 233209
-        },
-        {
-            city: 'Milwaukee',
-            population: 594833
-        },
-        {
-            city: 'Green Bay',
-            population: 104057
-        },
-        {
-            city: 'Superior',
-            population: 27244
-        }
-    ];
+    //create Albers equal area conic projection for US
+    var projection = d3.geoAlbersUsa()
 
-    var x = d3.scaleLinear() //create the scale
-    .range([90, 755]) //output min and max
-    .domain([0, 3]); //input min and max
+    //create a path generator    
+    var path = d3.geoPath()
+        .projection(projection);
 
-    //find the minimum value of the array
-    var minPop = d3.min(cityPop, function(d){
-        return d.population;
-    });
+    var promises = [];    
+    promises.push(d3.csv("data/LandUse_Percentage.csv")); //load attributes from csv     
+    promises.push(d3.json("data/cb_2018_us_state_5m.topojson")); //load choropleth spatial data    
+    Promise.all(promises).then(callback);
 
-    //find the maximum value of the array
-    var maxPop = d3.max(cityPop, function(d){
-        return d.population;
-    });
+    function callback(data) {
+        var csvData = data[0],
+            state = data[1];
+            
 
-    //scale for circles center y coordinate
-    var y = d3.scaleLinear()
-        .range([450, 50]) 
-        .domain([0, 700000]); 
+    var stateBoundaries = topojson.feature(state, state.objects.cb_2018_us_state_5m).features;
+    
+    var graticule = d3.geoGraticule()
+    .step([5, 5]); //place graticule lines every 5 degrees of longitude and latitude
 
-    //color scale generator 
-    var color = d3.scaleLinear()
-        .range([
-            "#FDBE85",
-            "#D94701"
-        ])
-        .domain([
-            minPop, 
-            maxPop
-        ]);
+    //create graticule background
+    var gratBackground = map.append("path")
+        .datum(graticule.outline()) //bind graticule background
+        .attr("class", "gratBackground") //assign class for styling
+        .attr("d", path) //project graticule
 
-
-    var circles = container.selectAll(".circles") //create an empty selection
-        .data(cityPop) //here we feed in an array
-        .enter() //one of the great mysteries of the universe
-        .append("circle") //inspect the HTML--holy crap, there's some circles there
-        .attr("class", "circles")
-        .attr("id", function(d){
-            return d.city;
-        })
-        .attr("r", function(d){
-            //calculate the radius based on population value as circle area
-            var area = d.population * 0.01;
-            return Math.sqrt(area/Math.PI);
-        })
-        .attr("cx", function(d, i){
-            //use the scale generator with the index to place each circle horizontally
-            return x(i)
-        })
-
-        .attr("cy", function(d){
-            return y(d.population);
-        })
-        .style("fill", function(d, i){ //add a fill based on the color scale generator
-            return color(d.population);
-        })  
-
-        .style("stroke", "#000"); //black circle stroke
-
-    var yAxis = d3.axisLeft(y);
-
-    //create axis g element and add axis
-    var axis = container.append("g")
-        .attr("class", "axis")
-        .attr("transform", "translate(50, 0)")
-        .call(yAxis);
-
-    var title = container.append("text")
-        .attr("class", "title")
-        .attr("text-anchor", "middle")
-        .attr("x", 4050)
-        .attr("y", 30)
-        .text("City Populations");
-
-    var labels = container.selectAll(".labels")
-        .data(cityPop)
+    var gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
+        .data(graticule.lines()) //bind graticule lines to each element to be created
+        .enter() //create an element for each datum
+        .append("path") //append each element to the svg as a path element
+        .attr("class", "gratLines") //assign class for styling
+        .attr("d", path); //project graticule lines
+        
+    //add states
+    var states = map.selectAll(".states")
+        .data(stateBoundaries)
         .enter()
-        .append("text")
-        .attr("class", "labels")
-        .attr("text-anchor", "left")
-        .attr("y", function(d){
-            //vertical position centered on each circle
-            return y(d.population) - 5;
-        });
-
-    //first line of label
-    var nameLine = labels.append("tspan")
-        .attr("class", "nameLine")
-        .attr("x", function(d,i){
-            //horizontal position to the right of each circle
-            return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
+        .append("path")
+        .attr("class", function(d){
+            return "states ";
         })
-        .text(function(d){
-            return d.city;
-        });
+        .attr("d", path);
+    console.log(states)
 
-    //create format generator
-    var format = d3.format(",");
-
-    //second line of label
-    var popLine = labels.append("tspan")
-        .attr("class", "popLine")
-        .attr("x", function(d,i){
-            return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
-        })
-        .attr("dy", "15") //vertical offset
-        .text(function(d){
-            return "Pop. " + format(d.population); //use format generator to format numbers
-        });
+    }
 };
+
+
